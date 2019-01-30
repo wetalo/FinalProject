5bcfb6bbd39b785a03eb6a32fdf389af194212c2
Daniel Swieca <danielswieca@gmail.com>
2019-01-30 12:52:42 -0500

    Adding UI

---
+++ FinalProject/Assets/Scripts/Hook/GrapplePointHitbox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplePointHitbox : MonoBehaviour {
+
+
+    public GameObject grapplePoint;
+    bool isBeingProjected;
+
+    public Vector3 EnableProjection()
+    {
+        grapplePoint.GetComponent<Renderer>().enabled = false;
+        isBeingProjected = true;
+
+        return grapplePoint.transform.position;
+    }
+
+    public void DisableProjection()
+    {
+        grapplePoint.GetComponent<Renderer>().enabled = true;
+        isBeingProjected = false;
+        
+    }
+
+}
--- FinalProject/Assets/Scripts/Hook/HookRenderer.cs
+++ FinalProject/Assets/Scripts/Hook/HookRenderer.cs
@@ -14,6 +14,9 @@ public class HookRenderer : MonoBehaviour {
 
     Vector3 targetPosition;
 
+    bool isBeingProjected = false;
+    GrapplePointHitbox currentGrapplePoint;
+
 	// Use this for initialization
 	void Start () {
         targetPrefabInstance = GameObject.Instantiate(targetPrefab);
@@ -35,10 +38,33 @@ public class HookRenderer : MonoBehaviour {
         {
             targetPosition = hit.point;
             //The object the projectile hit
-            targetPoint.SetValid(true);
+
+
+            if(hit.collider.gameObject.tag == "GrapplePoint")
+            {
+                isBeingProjected = true;
+                currentGrapplePoint = hit.collider.gameObject.GetComponent<GrapplePointHitbox>();
+                targetPosition = currentGrapplePoint.EnableProjection();
+                targetPoint.SetValid(true);
+            } else
+            {
+                if (isBeingProjected)
+                {
+                    currentGrapplePoint.DisableProjection();
+                    currentGrapplePoint = null;
+                    isBeingProjected = false;
+                }
+                targetPoint.SetValid(false);
+            }
         }
         else
         {
+            if (isBeingProjected)
+            {
+                currentGrapplePoint.DisableProjection();
+                currentGrapplePoint = null;
+                isBeingProjected = false;
+            }
             targetPosition = transform.position + transform.forward * maxDistance.Value;
             targetPoint.SetValid(false);
         }
